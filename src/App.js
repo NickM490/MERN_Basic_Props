92c58c7c9e77cc2b36f192eb59550def0a5bcb2d
Nick_M <nick@49studios.com>
2020-06-18 12:04:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './Components/PersonCard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <PersonCard firstName = "Jane" lastName = "Doe" Age = {88} hairColor = "Brown" />
+        <PersonCard firstName = "John" lastName = "Doe" Age = {22} hairColor = "Green" />
+        <PersonCard firstName = "Billy" lastName = "Goat" Age = {3154} hairColor = "Teal" />
+        <PersonCard firstName = "Jim" lastName = "Jones" Age = {55} hairColor = "432" />
+      </div>,
+
+      <button onClick = { ()=> alert("The alert button has been clicked")}>ALERT</button>
+      <button onFocus = { ()=> alert("The Focus button has been clicked")}>FOCUS</button>
+      <p onBlur = { ()=> alert("The Blur button has been clicked")}>BLUR</p>
+      </>
   );
 }
 
---
+++ src/Components/PersonCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>Name: {props.lastName}, {props.firstName}</h1>
+            <p>Age: {props.Age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+        </div>
+    );
+}
+
+export default PersonCard;
